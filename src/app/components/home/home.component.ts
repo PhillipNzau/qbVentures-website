f1646e61b93cc1b46851a741ecdace036cf67170
phillipnzau <nzauonline78@gmail.com>
2023-03-29 11:23:55 +0300

    Footer complete

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsCardComponent } from 'src/app/shared/news-card/news-card.component';
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule, NewsCardComponent],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    imports: [CommonModule, NewsCardComponent, FooterComponent]
 })
 export class HomeComponent {
 
