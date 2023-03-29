fc134965085be02d3cdd6b4d0eab55822868088b
phillipnzau <nzauonline78@gmail.com>
2023-03-29 08:06:39 +0300

    Routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path:'', loadComponent:() => import('./components/home/home.component').then(mod => mod.HomeComponent)
+  },
+  {
+    path: 'about', loadComponent:() => import('./components/about/about.component').then(mod => mod.AboutComponent)
+  },
+  {
+    path: 'capital', loadComponent:() => import('./components/capital/capital.component').then(mod => mod.CapitalComponent)
+  },
+  {
+    path: 'contact', loadComponent:() => import('./components/contact/contact.component').then(mod => mod.ContactComponent)
+  },
+  {
+    path: 'studio', loadComponent:() => import('./components/studio/studio.component').then(mod => mod.StudioComponent)
+  },
+  {
+    path: 'team', loadComponent:() => import('./components/team/team.component').then(mod => mod.TeamComponent)
+  },
+  
+  {
+    path: '**', redirectTo: '', pathMatch: 'full'
+  }
 ];
 
 @NgModule({
