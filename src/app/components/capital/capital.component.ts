7c731ab9eac4a38305950ccf0054ed3aee41329f
phillipnzau <nzauonline78@gmail.com>
2023-04-03 11:48:35 +0300

    capital and coming soon page

--- src/app/components/capital/capital.component.ts
+++ src/app/components/capital/capital.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
+import { NewsCardComponent } from "../../shared/news-card/news-card.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
-  selector: 'app-capital',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './capital.component.html',
-  styleUrls: ['./capital.component.scss']
+    selector: 'app-capital',
+    standalone: true,
+    templateUrl: './capital.component.html',
+    styleUrls: ['./capital.component.scss'],
+    imports: [CommonModule, NavbarComponent, NewsCardComponent, FooterComponent]
 })
 export class CapitalComponent {
+    items: number[] = [1,2,3,4,5,6,7,8,10,11,2,45,4,8,9,8,79,7];
 
 }
