6297d5c0177f609b7017d0c1551c07437e80242d
phillip <nzauonline@gmail.com>
2023-04-13 22:46:55 +0300

    home and about page update

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,14 +3,35 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
 import { NewsCardComponent } from "../../shared/news-card/news-card.component";
+import { TeamCardComponent } from "../../shared/team-card/team-card.component";
 
 @Component({
     selector: 'app-about',
     standalone: true,
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.scss'],
-    imports: [CommonModule, NavbarComponent, FooterComponent, NewsCardComponent]
+    imports: [CommonModule, NavbarComponent, FooterComponent, NewsCardComponent, TeamCardComponent]
 })
 export class AboutComponent {
+    team = [
+        {
+            name: 'Aaron Nzau',
+            title: 'Founder and CEO',
+            image:'./assets/imgs/aaron.webp',
+            social: 'linkedin'
+        },
+        {
+            name: 'Richard Otolo',
+            image:'./assets/imgs/richard.webp',
+            title: 'CTO',
+            social: 'linkedin'
+        },
+        {
+            name: 'Purity Kimani',
+            title: 'CFO',
+            image:'./assets/imgs/aaron.webp',
+            social: 'linkedin'
+        }
+    ]
 
 }
