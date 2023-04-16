fd8e07a4547769818b15bed2bf4f09f94d6bc826
phillip <nzauonline@gmail.com>
2023-04-16 18:03:08 +0300

    fix2

--- src/app/components/capital/capital.component.ts
+++ src/app/components/capital/capital.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import { NewsCardComponent } from "../../shared/news-card/news-card.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-capital',
     standalone: true,
     templateUrl: './capital.component.html',
     styleUrls: ['./capital.component.scss'],
-    imports: [CommonModule, NavbarComponent, NewsCardComponent, FooterComponent]
+    imports: [CommonModule, NavbarComponent, NewsCardComponent, FooterComponent, RouterLink]
 })
 export class CapitalComponent {
     items: number[] = [1,2,3,4,5,6,7,8,10,11,2,45,4,8,9,8,79,7];
