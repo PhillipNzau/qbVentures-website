d1eda7a84c84e6ca65d534eb431e5b32aff67c6f
phillip <nzauonline@gmail.com>
2023-04-16 17:03:22 +0300

    updates

--- src/app/components/studio/studio.component.ts
+++ src/app/components/studio/studio.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
     selector: 'app-studio',
     standalone: true,
     templateUrl: './studio.component.html',
     styleUrls: ['./studio.component.scss'],
-    imports: [CommonModule, NavbarComponent, FooterComponent]
+    imports: [CommonModule, NavbarComponent, FooterComponent, RouterLink, RouterLinkActive]
 })
 export class StudioComponent {
 
