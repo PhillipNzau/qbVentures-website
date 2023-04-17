b687129d5fd3e6fc5540c2524b7db5fd4a68b977
phillipnzau <nzauonline78@gmail.com>
2023-04-17 10:20:19 +0300

    auto close details

--- src/app/components/capital/capital.component.ts
+++ src/app/components/capital/capital.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import { NewsCardComponent } from "../../shared/news-card/news-card.component";
@@ -12,7 +12,26 @@ import { RouterLink } from '@angular/router';
     styleUrls: ['./capital.component.scss'],
     imports: [CommonModule, NavbarComponent, NewsCardComponent, FooterComponent, RouterLink]
 })
-export class CapitalComponent {
+export class CapitalComponent implements OnInit {
     items: number[] = [1,2,3,4,5,6,7,8,10,11,2,45,4,8,9,8,79,7];
+
+    // Fetch all the details element.
+    details:any
+
+    ngOnInit() {
+        this.details = document.querySelectorAll("details");
+        // Add the onclick listeners.
+        this.details.forEach((targetDetail:any) => {            
+            targetDetail.addEventListener("click", () => {
+            // Close all the details that are not targetDetail.
+            this.details.forEach((detail:any) => {
+                if (detail !== targetDetail) {
+                detail.removeAttribute("open");
+                }
+            });
+            });
+        });
+    }
+
 
 }
